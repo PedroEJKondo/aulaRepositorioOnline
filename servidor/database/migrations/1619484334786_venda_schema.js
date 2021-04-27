1d450111a4e4884a5a8d5a170a4768c03e6c52fc
pedro kondo <pedrokondo20@gmail.com>
2021-04-27 08:48:36 +0100

    Migração para venda com foreign

--- servidor/database/migrations/1619484334786_venda_schema.js
+++ servidor/database/migrations/1619484334786_venda_schema.js
@@ -4,16 +4,20 @@
 const Schema = use('Schema')
 
 class VendaSchema extends Schema {
-  up () {
-    this.create('vendas', (table) => {
-      table.increments()
-      table.timestamps()
-    })
-  }
+    up() {
+        this.create('vendas', (table) => {
+            table.increments()
+            table.string('cliente')
+            table.integer('qtd')
+            table.integer('produto_id').unsigned();
+            table.foreign('produto_id').references('id').inTable('produtos');
+            table.timestamps()
+        })
+    }
 
-  down () {
-    this.drop('vendas')
-  }
+    down() {
+        this.drop('vendas')
+    }
 }
 
 module.exports = VendaSchema
