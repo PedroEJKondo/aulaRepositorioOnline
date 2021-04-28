4c89a76add4b1f5e118f7989bbae36814f191190
pedro kondo <pedrokondo20@gmail.com>
2021-04-28 20:42:20 +0100

    add utilizador

--- cliente/src/app/core/service/produto.service.ts
+++ cliente/src/app/core/service/produto.service.ts
@@ -1,7 +1,5 @@
 import { HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-import { Produto } from 'src/app/models/Produto';
 import { ApiServiceService } from '../provide/http/api-service.service';
 @Injectable({
   providedIn: 'root'
@@ -16,13 +14,9 @@ export class ProdutoService {
 
   public show(params:any):any{return this.api.get('/produtos/',params);}
 
-  public post(produto):any{
-    return this.api.post('/produtos',produto);
-  }
+  public post(produto):any{ return this.api.post('/produtos',produto); }
 
-  public put(form: FormGroup, params):any{
-    return this.api.put('/produtos/'+params,form.value);
-  }
+  public put(produto, params):any{ return this.api.put('/produtos/'+params,produto); }
   
   public delete(params):any{return this.api.delete('/produtos/'+params);}
 }
